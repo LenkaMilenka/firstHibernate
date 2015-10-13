0c8b05a68c1d6f16db0d638b458a312592f65c38
LenkaMilenka <lenakorccak@mail.ru>
2015-10-13 16:51:43 +0300

    end tuesday commit

--- HibernateSimpleExample/src/DAO/Impl/StudentDAOImpl.java
+++ HibernateSimpleExample/src/DAO/Impl/StudentDAOImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.criterion.Expression;
 
@@ -22,8 +23,8 @@ public class StudentDAOImpl implements StudentDAO {
 	 * Addition student to database
 	 * 
 	 * @see DAO.StudentDAO#addStudent(logic.Student)
-	 * @param stud
-	 *            - student
+	 * @param stud - student
+	 * @throws SQLException
 	 */
 	public void addStudent(Student stud) throws SQLException {
 		Session session = null;
@@ -46,8 +47,8 @@ public class StudentDAOImpl implements StudentDAO {
 	 * update student in the database
 	 * 
 	 * @see DAO.StudentDAO#updateStudent(logic.Student)
-	 * @param stud
-	 *            - student
+	 * @param stud - student
+	 * @throws SQLException
 	 */
 	public void updateStudent(Student stud) throws SQLException {
 		Session session = null;
@@ -70,8 +71,9 @@ public class StudentDAOImpl implements StudentDAO {
 	 * Get student by id
 	 * 
 	 * @see DAO.StudentDAO#getStudentById(int)
-	 * @param id
-	 *            - student's id
+	 * @param id - student's id
+	 * @return student's by id
+	 * @throws SQLException
 	 */
 	public Student getStudentById(int id) throws SQLException {
 		Session session = null;
@@ -95,6 +97,7 @@ public class StudentDAOImpl implements StudentDAO {
 	 * Get list all students
 	 * 
 	 * @see DAO.StudentDAO#getAllStudents()
+	 * @throws SQLException
 	 */
 	public List<Student> getAllStudents() throws SQLException {
 		Session session = null;
@@ -117,8 +120,8 @@ public class StudentDAOImpl implements StudentDAO {
 	 * delete student in the database
 	 * 
 	 * @see DAO.StudentDAO#deleteStudent(logic.Student)
-	 * @param stud
-	 *            - srudent
+	 * @param stud - student
+	 * @throws SQLException
 	 */
 	public void deleteStudent(Student stud) throws SQLException {
 		Session session = null;
@@ -141,8 +144,9 @@ public class StudentDAOImpl implements StudentDAO {
 	 * Gel list students by age = @param age
 	 * 
 	 * @see DAO.StudentDAO#selectUserByAge(int)
-	 * @param age
-	 *            - age of student
+	 * @param age - age of student
+	 * @return student's list by age
+	 * @throws SQLException
 	 */
 	@SuppressWarnings("deprecation")
 	public List<Student> selectUserByAge(int _age) throws SQLException {
@@ -166,6 +170,8 @@ public class StudentDAOImpl implements StudentDAO {
 	/** Get list students by name%
 	 * @see DAO.StudentDAO#selectUSerByName(java.lang.String)
 	 * @param _name - student's name
+	 * @return student's list by name%
+	 * @throws SQLException
 	 */
 	public List<Student> selectUSerByName(String _name) throws SQLException {
 		Session session = null;
@@ -185,16 +191,18 @@ public class StudentDAOImpl implements StudentDAO {
 	}
 	
 	/**
-	 * Get list through SQLQuery
+	 * Get student's list by name through SQLQuery
 	 * @return list with students
 	 * @throws SQLException
 	 */
-	public List<Student> selectSQLExecute() throws SQLException{
+	public List<Student> selectSQLExecute(String _name) throws SQLException{
 		List<Student> studs = new ArrayList<Student>();
 		Session session = null;
 		try{
 			session = HibernateUtil.getSessionFactory().openSession();					
 			session.createSQLQuery("select * from Student").addEntity(Student.class).list();
+			Query query = session.createSQLQuery("select * from Student where name like :name").addEntity(Student.class);
+			List<Student> result = query.setString("name", _name + "%").list();
 		}catch (Exception e) {
 			System.out.println(e);
 			JOptionPane.showMessageDialog(null, e.getMessage(), "Œ¯Ë·Í‡ I/O", JOptionPane.OK_OPTION);
